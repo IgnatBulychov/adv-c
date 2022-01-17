7808c521b7e37d0fe9fdad31dedeb08a19e3b805
IgnatBulychov <jose220593@gmail.com>
2022-01-17 22:49:52 +0300

    add search

---
+++ components/areas/AreasListGuest.vue
@@ -0,0 +1,82 @@
+<template>
+<div>
+  <v-card 
+      v-for="area in areas"
+      :key="area.id"
+      class="my-2 mx-2">
+      <v-list-item three-line>
+        <v-list-item-avatar
+          tile
+          size="100"
+        >
+          <v-img :src="area.poster"></v-img>
+          
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title class="text-h5 mb-1">            
+            {{ area.title }}
+            
+          <!--
+            <p class="subtitle-1">
+              {{ area.description }}
+            </p>
+-->
+            <p  class="subtitle-2">
+            
+                <v-icon class="mr-1" color="yellow">
+                  mdi-star
+                </v-icon>
+                <span class="subheading mr-2">256</span>
+              
+            </p>
+          </v-list-item-title>
+          <v-list-item-subtitle color="gray">
+            <v-avatar
+              class="mr-2"
+              size="24"
+            >
+              <v-img :src="area.network.poster"></v-img>
+            </v-avatar>
+            {{ area.network.title }}           
+          </v-list-item-subtitle>
+        </v-list-item-content> 
+
+        <div class="actions">
+          <div>        
+          </div>
+          
+        </div>   
+      </v-list-item>
+
+
+    </v-card>
+</div>
+</template>
+
+<script>
+
+export default {
+  props: ['areas'],
+  name: 'AreasList',
+  components: {
+    
+  },
+  data: () => ({
+
+  }),
+  methods: {   
+    
+  }
+}
+</script>
+
+<style scoped>
+.actions {
+  height: 110px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: flex-end;
+}
+</style>
--- pages/search.vue
+++ pages/search.vue
@@ -1,29 +1,104 @@
 <template>
-  <div>
-    <areas-list :areas="areas"/>
-  </div>
+   <v-container fluid>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              
+              
+<v-list
+      flat
+    >
+      <v-subheader>Размещение</v-subheader>
+
+      <v-list-item-group
+        v-model="selectedNetworks"
+        multiple
+        active-class=""
+        @change="findAreas"
+      >
+        <v-list-item v-for="network in networks" :key="network.id" :value="network.id">
+          <template v-slot:default="{ active }">
+            <v-list-item-action>
+              <v-checkbox :input-value="active"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-avatar size="20">
+              <v-img :src="network.poster"></v-img>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ network.title }}</v-list-item-title>
+            </v-list-item-content>
+
+            
+          </template>
+        </v-list-item>
+
+        
+      </v-list-item-group>
+    </v-list>
+
+
+
+
+
+            
+            
+            </v-sheet> 
+          </v-col>
+
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+              class="pb-1"
+            >
+              
+              
+
+    <areas-list-guest :areas="areas"/>
+
+
+
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
 </template>
 
 <script>
 
-import AreasList from '~/components/areas/AreasList'
+import AreasListGuest from '~/components/areas/AreasListGuest'
 export default {
   layout: 'index',
   components: {
-    AreasList
+    AreasListGuest
   },
   data: () => ({
+    selectedNetworks:[],
+    networks: [],
     areas:[],
   }),
   methods: {
     async findAreas() {
-      let res = await this.$axios.get(`/areas/search`, {
-        a:1
-      })
-      this.areas = res.data
+      let params = {}
+      if (this.selectedNetworks.length) params.networksIds = this.selectedNetworks.join(',')
+      try {
+        let res = await this.$axios.get(`/areas/search`, {
+          params: params
+        })
+        this.areas = res.data
+      } catch {
+
+      }
+    },
+    async findNetwork() {
+      let res = await this.$axios.get(`/networks`)
+      this.networks = res.data
     },
   },
-  mounted() {
+  async mounted() {
+    await this.findNetwork()
     this.findAreas()
   }
 }
